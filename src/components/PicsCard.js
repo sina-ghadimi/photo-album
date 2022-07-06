07f00f4afdac5d04286edddee41c5ce4f381d4ad
sina ghadimi <sinaghadimi92@gmail.com>
2022-07-06 20:19:08 +0430

    render each pic in Card using PicsCard

---
+++ src/components/PicsCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class PicsCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = { spans: 0 };
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+  render() {
+    const { description, urls } = this.props.pic;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    );
+  }
+}
+
+export default PicsCard;
