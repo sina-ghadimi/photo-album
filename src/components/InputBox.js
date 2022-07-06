b8fafcbed1210f8a2fd9099a1b94cb7f8b48fb4e
sina ghadimi <sinaghadimi92@gmail.com>
2022-07-06 20:09:53 +0430

    make inputbox component

---
+++ src/components/InputBox.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Field, Form } from "react-final-form";
+
+const InputBox = (props) => {
+  const renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  const onSubmit = (formValues) => {
+    props.onSubmit(formValues);
+  };
+
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {renderError(meta)}
+      </div>
+    );
+  };
+  return (
+    <Form
+      initialValues={props.initialValues}
+      onSubmit={onSubmit}
+      validate={(formValues) => {
+        const errors = {};
+
+        if (!formValues.title) {
+          errors.title = "Please Enter a Keyword";
+        }
+
+        return errors;
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} className="ui form error">
+          <Field name="title" component={renderInput} label="Enter Term" />
+          <button className="ui button primary">Submit</button>
+        </form>
+      )}
+    ></Form>
+  );
+};
+
+export default InputBox;
