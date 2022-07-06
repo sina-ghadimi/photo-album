9e7a8b9b90feb1eaaeaf406c7aae1d1f16eb8e1b
sina ghadimi <sinaghadimi92@gmail.com>
2022-07-06 20:43:52 +0430

    finalize Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import InputBox from "./InputBox";
+import PicsRender from "./PicsRender";
+import { fetchPics } from "./../actions";
+import { connect } from "react-redux";
 
 class App extends React.Component {
-  render(){
-    return <div></div>
+  onSubmit = (formValues) => {
+    this.props.fetchPics(formValues);
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <InputBox onSubmit={this.onSubmit} />
+        <PicsRender />
+      </div>
+    );
   }
 }
 
-export default App
+export default connect(null, { fetchPics })(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from ".components/App";
+import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import reducers from "./reducers";
 
+const store = createStore(reducers, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
